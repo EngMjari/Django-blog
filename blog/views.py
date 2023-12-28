9dc235f6f06c023377fff7c7d50a3a2167923445
EngMJari <mohammad.jarii@gmail.com>
2023-12-28 21:22:52 +0330

    Create First View

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def homePage(request):
+  template = "pages/index.html"
+  context = {
+    
+  }
+  return render(request, template, context)
