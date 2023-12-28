5cacf5b7fd8b834f3db0967b0f035af578864b14
EngMJari <mohammad.jarii@gmail.com>
2023-12-29 00:50:42 +0330

    create Category and BlogPost Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+
+class BlogPost(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    context = models.TextField()
+    is_published = models.BooleanField(default=False)
+    date_published = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="post-image/")
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
