2a80331e55fcd9530911b3b0e30590a8f2011e3b
EngMJari <mohammad.jarii@gmail.com>
2023-12-29 00:03:25 +0330

    Create View and fix navbar Link

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
-from .views import homePage
+from .views import (
+    homePage, 
+    blogView,
+    categoryView,
+    LoginView,
+    registerView,
+)
 urlpatterns = [
     path("", homePage, name="Homepage"),
+    path("blog/", blogView, name="blogPage"),
+    path("category/<slug:slug>/", categoryView, name="categoryPage"),
+    path("login/", LoginView, name="loginPage"),
+    path("register/", registerView, name="registerPage"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 
+
 def homePage(request):
-  template = "pages/index.html"
-  context = {
-    
-  }
-  return render(request, template, context)
+    template = "pages/index.html"
+    context = {}
+    return render(request, template, context)
+
+
+def blogView(request):
+    template = "pages/blog.html"
+    context = {}
+    return render(request, template, context)
+
+
+def categoryView(request, slug="salam"):
+    template = "pages/category.html"
+    context = {
+      "title": "slug",
+    }
+    return render(request, template, context)
+
+
+def LoginView(request):
+    template = "pages/login.html"
+    context = {}
+    return render(request, template, context)
+
+
+def registerView(request):
+    template = "pages/register.html"
+    context = {}
+    return render(request, template, context)
